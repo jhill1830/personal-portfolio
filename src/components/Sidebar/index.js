2cfedab7deebc9dd1617d73340b70262078d5989
James <36040856+jhill1830@users.noreply.github.com>
2022-04-19 22:23:26 +1000

    navbar now working without compile error

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,12 +1,25 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
 import LogoS from '../../logo.svg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => (
     <div className='nav-bar'>
         <Link className = 'logo' to = '/'>
             <img src = {LogoS} alt = "logo" />
         </Link>
+        <nav>
+            <NavLink exact="true" aciveclassname="active" to = "/">
+                <FontAwesomeIcon icon={faHome} color = "#4d4d4e" />
+            </NavLink>
+            <NavLink exact="true" aciveclassname="active" className = "about-link" to = "/about">
+                <FontAwesomeIcon icon={faUser} color = "#4d4d4e" />
+            </NavLink>
+            <NavLink exact="true" aciveclassname="active" className = "contact-link" to = "/contact">
+                <FontAwesomeIcon icon={faEnvelope} color = "#4d4d4e" />
+            </NavLink>
+        </nav> 
     </div>
     )
 
