8f6a2315681c8da83025f70f5cb76fa6ed7789d9
James <36040856+jhill1830@users.noreply.github.com>
2022-04-23 19:47:43 +1000

    font editing

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,7 +3,8 @@ import './index.scss'
 import LogoS from '../../logo.svg'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faHome, faUser} from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
     <div className='nav-bar'>
@@ -21,7 +22,19 @@ const Sidebar = () => (
             <NavLink exact="true" aciveclassname="active" className = "contact-link" to = "/contact">
                 <FontAwesomeIcon icon={faEnvelope} color = "#4d4d4e" />
             </NavLink>
-        </nav> 
+        </nav>
+        <ul>
+            <li>
+                <a target = "_blank" rel="noreferror" href="">
+                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a target = "_blank" rel="noreferror" href="https://github.com/jhill1830/">
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+            </li>
+        </ul>
     </div>
     )
 
