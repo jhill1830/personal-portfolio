c67df1cecc080dad9b813a26df12e2eb6e171911
James <36040856+jhill1830@users.noreply.github.com>
2022-04-25 22:39:03 +1000

    css and animated homepage

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,19 @@
-import LogoTitle from '../../assets/images/logo-s.png'
+import { Link } from 'react-router-dom';
+import LogoTitle from '../../assets/images/logo-s.png';
+import './index.scss';
 
 const Home = () => {
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br /> I'm</h1>
+                <h1>Hi, <br /> I'm
                 <img src={LogoTitle} alt="developer" />
+                lobodan
+                <br />
+                web developer
+                </h1>
+                <h2>Frontend Developer / Javascipt Expert / YouTuber</h2>
+                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
             </div>
         </div>
     )
