02407665d835572a94ad6cf158ff68b379f973bb
James <36040856+jhill1830@users.noreply.github.com>
2022-05-04 01:50:51 +1000

    more navbar editing. moved links to bottom of page

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,39 +3,46 @@ import './index.scss'
 import LogoS from '../../logo.svg'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faHome, faUser} from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faGear, faGears, faHome, faUser} from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Navbar = () => (
-    <div className='nav-bar'>
-        <Link className = 'logo' to = '/'>
-            <img src = {LogoS} alt = "logo" />
-            <img className="sub-logo" src = {LogoSubtitle} alt = "James H" />
-        </Link>
-        <nav>
-            <NavLink exact="true" aciveclassname="active" className='nav-links' to = "/">
-                <FontAwesomeIcon icon={faHome} color = "#4d4d4e" />
-            </NavLink>
-            <NavLink exact="true" aciveclassname="active" className = "nav-links about-link" to = "/about">
-                <FontAwesomeIcon icon={faUser} color = "#4d4d4e" />
-            </NavLink>
-            <NavLink exact="true" aciveclassname="active" className = "nav-links contact-link" to = "/contact">
-                <FontAwesomeIcon icon={faEnvelope} color = "#4d4d4e" />
-            </NavLink>
-        </nav>
-        <ul>
-            <li>
-                <a target = "_blank" rel="noreferror" href="">
-                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
-                </a>
-            </li>
-            <li>
-                <a target = "_blank" rel="noreferror" href="https://github.com/jhill1830/">
-                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
-                </a>
-            </li>
-        </ul>
-    </div>
+    <>
+        <div className='nav-bar'>
+            <Link className = 'logo' to = '/'>
+                <img src = {LogoS} alt = "logo" />
+                <img className="sub-logo" src = {LogoSubtitle} alt = "James H" />
+            </Link>
+            <nav>
+                <NavLink exact="true" aciveclassname="active" className='nav-links' to = "/">
+                    <FontAwesomeIcon icon={faHome} color = "#4d4d4e" />
+                </NavLink>
+                <NavLink exact="true" aciveclassname="active" className = "nav-links about-link" to = "/about">
+                    <FontAwesomeIcon icon={faUser} color = "#4d4d4e" />
+                </NavLink>
+                <NavLink exact="true" aciveclassname="active" className = "nav-links skills-link" to = "/skills">
+                    <FontAwesomeIcon icon={faGear} color = "#4d4d4e" />
+                </NavLink>
+                <NavLink exact="true" aciveclassname="active" className = "nav-links contact-link" to = "/contact">
+                    <FontAwesomeIcon icon={faEnvelope} color = "#4d4d4e" />
+                </NavLink>
+            </nav>
+        </div>
+        <div className='bottom-links'>
+            <ul>
+                <li>
+                    <a target = "_blank" rel="noreferror" href="">
+                        <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                    </a>
+                </li>
+                <li>
+                    <a target = "_blank" rel="noreferror" href="https://github.com/jhill1830/">
+                        <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </>
     )
 
 
